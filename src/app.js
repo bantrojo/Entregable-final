74b59f761b75a9e0f9a5f860ad4101d05a0fa240
Nicolas Baumtrog <98490360+bantrojo@users.noreply.github.com>
2022-08-02 13:11:37 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -2,36 +2,32 @@ const express = require('express');
 const productRouter = require('./routes/productRouter');
 const cartRouter = require('./routes/cartRouter');
 const userRouter = require('./routes/userRouter');
-const path = require('path');
+const path=require('path');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const mongoose = require('mongoose');
+const mongoose=require('mongoose');
 require('dotenv').config();
 ///graph
-const { graphqlHTTP } = require("express-graphql");
-const { buildSchema } = require("graphql");
-
-const morgan = require('morgan');
-const crypto = require("crypto")
-const engine = require('ejs-mate');
+const {graphqlHTTP}=require("express-graphql");
+const {buildSchema}=require("graphql");
+const morgan=require('morgan');
+const crypto=require("crypto")
+const engine=require('ejs-mate');
 const passport = require('passport');
-const localp=require('./passport/local-auth');
-const flash = require('connect-flash');
 
 
 
 const app = express();
 require('./database');
-require('./passport/local-auth');
-require('./config/emailer');
 
 
 //engine settings
-app.set('views', path.join(__dirname, 'views'));
-app.engine('ejs', engine);
-app.set('view engine', 'ejs');
+app.set('views',path.join(__dirname,'views'));
+app.engine('ejs',engine);
+app.set('view engine','ejs');
 
 app.use(morgan('dev'));
+app.use
 
 const admin = true;
 app.use(express.urlencoded({ extended: true }));
@@ -39,27 +35,16 @@ app.use(express.json());
 app.use(express.static(__dirname + '/public'));
 app.use(cookieParser());
 app.use(session({
-    secret: 'thisisverysecret',
-    resave: false,
-    saveUninitialized: false
+    secret:'thisisverysecret',
+    resave:false,
+    saveUninitialized:false
 }));
-app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(passport.initialize());
-app.use((req, res, next) => {
-    app.locals.signupMessage = req.flash('signupMessage');
-    app.locals.signinMessage = req.flash('signinMessage');
-    app.locals.user=req.user;
-    next();
-});
-
-
 app.use('/', productRouter);
 app.use('/', cartRouter);
 // app.use('/', userRouter);
-
-app.use('/', require('./routes/index'));
+app.use('/',require('./routes/index'));
 
 // //prueba de con
 // app.get('/', (req, res) => {
